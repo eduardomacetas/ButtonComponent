e24147b34919096765d556d921fc01808e33612f
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-09-25 08:02:26 -0500

    Adding constants file

---
+++ src/shared/constants.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const HEIGHT_SIZES = {
+  sm: '32px',
+  md: '36px',
+  lg: '42px'
+}
+
+export const WIDTH_SIZES = {
+  sm: '73px',
+  md: '81px',
+  lg: '91px'
+}
+
+export const COLORS = {
+  default: '#E0E0E0',
+  primary: '#2962FF',
+  secondary: '#455A64',
+  danger:  '#D32F2F',
+  darkGray: '#3F3F3F',
+  white: '#fff'
+}
