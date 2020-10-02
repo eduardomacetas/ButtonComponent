e40741e4ab026e3753365ade67df446b405610c4
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-10-01 22:24:04 -0500

    Creating my Text Component

---
+++ src/component/Text/Text.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components'
+import { SIZES, COLORS, FONT_WEIGHTS } from '../../shared/constants';
+
+const StyledText = styled.div`{
+  color: ${({color}) => (color ? COLORS[color] : '#333333')};
+  font-size: ${({size}) => (size ? SIZES[size] : '12px')};
+  font-style: normal;
+  font-weight: ${({weight}) => (weight ? FONT_WEIGHTS[weight] : 'normal')};
+  };
+`
+
+function Text(props) {
+  console.log("paola",props)
+  return <StyledText {...props}> {props.children} </StyledText>
+}
+
+Text.defaultProps = {
+  size: 'medium',
+  color: '#333333',
+  weight: 'normal'
+}
+
+export default Text;
