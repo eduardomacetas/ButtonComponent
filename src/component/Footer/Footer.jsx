4b830defa553571e857a6d6e544e57032d6177da
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-10-10 09:39:47 -0500

    Creating my Footer component

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+  render () {
+    return (
+      <div className="footer">
+        Eduardo Macetas <a href="https://devchallenges.io/"> @devchallenges.io</a>
+      </div>
+    )
+  }
+}
+
+export default Footer;
