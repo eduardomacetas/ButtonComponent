7e2e6ec7add1180fbffd7608c1d853705fb037dd
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-10-14 23:26:57 -0500

    Creating ShowButton component

---
+++ src/component/ShowButton/ShowButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ShowButton.css';
+
+class ShowButton extends React.Component {
+  render () {
+    return (
+      <div className="showbutton">
+        Botones
+      </div>
+    )
+  }
+}
+
+export default ShowButton;
