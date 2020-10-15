bb08afc178ac7d26b2c55e2057f72501edef4098
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-10-14 23:26:27 -0500

    Creating Header component

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  render () {
+    return (
+      <div className="header">
+        My header
+      </div>
+    )
+  }
+}
+
+export default Header;
