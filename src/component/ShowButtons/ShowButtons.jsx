28bbc0911c566208c8937aa38cfffa765ddb9b23
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-10-21 22:38:07 -0500

    Adding ShowButtons component

---
+++ src/component/ShowButtons/ShowButtons.jsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import './ShowButtons.css';
+import Label from '../Label/Label';
+import Button from '../Button';
+
+class ShowButtons extends React.Component {
+  render () {
+    return (
+      <div className="main"> 
+        <div className="show-buttons">
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+        </div>
+
+        <div className="show-buttons">
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+        </div>
+
+        <div className="show-buttons">
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+        </div>
+
+        <div className="show-buttons">
+          <div className="show-button">
+              <Label style={{marginBottom: '12px'}}> button </Label>
+              <Button />
+          </div>
+        </div>
+
+        <div className="show-buttons">
+          <div className="show-button">
+              <Label style={{marginBottom: '12px'}}> button </Label>
+              <Button />
+          </div>
+          <div className="show-button">
+              <Label style={{marginBottom: '12px'}}> button </Label>
+              <Button />
+          </div>
+        </div>
+
+        <div className="show-buttons">
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+        </div>
+
+        <div className="show-buttons">
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+        </div>
+
+        <div className="show-buttons">
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+        </div>
+
+        <div className="show-buttons">
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+          <div className="show-button">
+            <Label style={{marginBottom: '12px'}}> button </Label>
+            <Button />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ShowButtons;
