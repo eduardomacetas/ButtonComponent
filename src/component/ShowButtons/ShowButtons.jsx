3bf6a85020f32116ccc56a68e09ab9824e581b2e
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-11-17 23:20:24 -0500

    Removing style from all "Label"

--- src/component/ShowButtons/ShowButtons.jsx
+++ src/component/ShowButtons/ShowButtons.jsx
@@ -9,11 +9,11 @@ class ShowButtons extends React.Component {
       <div className="main"> 
         <div className="show-buttons">
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text="<Button />"/>
+            <Label text="<Button />"/>
             <Button />
           </div>
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text="&:hover, &:focus" color="gray"/>
+            <Label text="&:hover, &:focus" color="gray"/>
             <Button />
           </div>
           <div className="show-button">
@@ -22,92 +22,92 @@ class ShowButtons extends React.Component {
 
         <div className="show-buttons">
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button variant="outline" />'/>
+            <Label text='<Button variant="outline" />'/>
             <Button variant="outline"/>
           </div>
-          <div className="show-button"> 
-            <Label style={{marginBottom: '12px'}} text="&:hover, &:focus" color="gray"/>
+          <div className="show-button">
+            <Label text="&:hover, &:focus" color="gray"/>
             <Button variant="text"/>
           </div>
         </div>
 
         <div className="show-buttons">
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button variant="text" />'/>
+            <Label text='<Button variant="text" />'/>
             <Button variant="text"/>
           </div>
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text="&:hover, &:focus" color="gray"/>
+            <Label text="&:hover, &:focus" color="gray"/>
             <Button variant="text"/>
           </div>
         </div>
 
         <div className="show-buttons">
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button disableShadow />'/>
+            <Label text='<Button disableShadow />'/>
             <Button disableShadow/>
           </div>
         </div>
 
         <div className="show-buttons">
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button disabled />'/>
+            <Label text='<Button disabled />'/>
             <Button disabled/>
           </div>
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button variant="text" disabled />'/>
+            <Label text='<Button variant="text" disabled />'/>
             <Button variant="text" disabled/>
           </div>
         </div>
 
         <div className="show-buttons">
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button startIcon="local_grocery_store" />'/>
+            <Label text='<Button startIcon="local_grocery_store" />'/>
             <Button startIcon="local_grocery_store" />
           </div>
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button endIcon="local_grocery_store" />'/>
+            <Label text='<Button endIcon="local_grocery_store" />'/>
             <Button endIcon="local_grocery_store" />
           </div>
         </div>
 
         <div className="show-buttons">
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button size="sm" />'/>
+            <Label text='<Button size="sm" />'/>
             <Button size="sm"/>
           </div>
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button size="md" />'/>
+            <Label text='<Button size="md" />'/>
             <Button size="md"/>
           </div>
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button size="lg" />'/>
+            <Label text='<Button size="lg" />'/>
             <Button size="lg"/>
           </div>
         </div>
 
         <div className="show-buttons">
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button color="default" />'/>
+            <Label text='<Button color="default" />'/>
             <Button color="default"/>
           </div>
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button color="primary" />'/>
+            <Label text='<Button color="primary" />'/>
             <Button color="primary"/>
           </div>
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button color="secondary" />'/>
+            <Label text='<Button color="secondary" />'/>
             <Button color="secondary"/>
           </div>
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text='<Button color="danger" />'/>
+            <Label text='<Button color="danger" />'/>
             <Button color="danger"/>
           </div>
         </div>
 
         <div className="show-buttons">
           <div className="show-button">
-            <Label style={{marginBottom: '12px'}} text="&:hover, &:focus" color="gray"/>
+            <Label text="&:hover, &:focus" color="gray"/>
             <Button color="default"/>
           </div>
           <div className="show-button">
